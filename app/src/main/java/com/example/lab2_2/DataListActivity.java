28f1f43442f00f72533ce82995621eb4752ac41c
Redrick58 <34249211+Redrick58@users.noreply.github.com>
2023-06-07 19:22:29 +0300

    initial commit

--- app/src/main/java/com/example/lab2_2/DataListActivity.java
+++ app/src/main/java/com/example/lab2_2/DataListActivity.java
@@ -8,7 +8,11 @@ import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.content.SharedPreferences;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import android.content.Context;
 
 
 public class DataListActivity extends AppCompatActivity {
@@ -17,14 +21,30 @@ public class DataListActivity extends AppCompatActivity {
     ListView list;
     int last_pos = 0;
     ArrayAdapter<String> adapter;
+    SharedPreferences sharedPreferences;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("name").toString();
         setContentView(R.layout.activity_data_list);
         input = (EditText) findViewById(R.id.editTextTextPersonName2);
         list = (ListView) findViewById(R.id.listview1);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         list.setAdapter(adapter);
 
+        // Инициализация SharedPreferences
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+
+        // Загрузка сохраненных данных из SharedPreferences
+        Set<String> savedData = sharedPreferences.getStringSet("dataList", null);
+        if (savedData != null) {
+            adapter.addAll(savedData);
+            adapter.notifyDataSetChanged();
+        }
+
+        adapter.add(name);
+        adapter.notifyDataSetChanged();
         list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id)
@@ -32,6 +52,33 @@ public class DataListActivity extends AppCompatActivity {
                 last_pos = position;
 
             } });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Set<String> strings_to_save = new HashSet<String>();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            String item = (String) adapter.getItem(i);
+            strings_to_save.add(item);
+        }
+        // Сохранение данных из ListView в SharedPreferences
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putStringSet("dataList", strings_to_save);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Загрузка сохраненных данных из SharedPreferences
+        Set<String> savedData = sharedPreferences.getStringSet("dataList", null);
+        if (savedData != null) {
+            adapter.clear();
+            adapter.addAll(savedData);
+            adapter.notifyDataSetChanged();
+        }
     }
 
     public void addToList(View view) {
